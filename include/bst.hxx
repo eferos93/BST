5ee9f0362964b5e0771ffd147cc5b66632958539
Eros <eros.fabrici@gmail.com>
2020-02-12 13:43:05 +0100

    constructors made; merged node_iterator branch

--- include/bst.hxx
+++ include/bst.hxx
@@ -19,6 +19,39 @@ class bst
         std::unique_ptr<Node> root;
         CompareType comparator;
     public:
+        /**
+         * @brief Default constructor
+         */
+        bst() {}
+        
+        /**
+         * @brief Constructor with node
+         * @param data The root of the bst to be created
+         * @param c The instance of the CompareType
+         */
+        bst(std::pair<KeyType, ValueType>& data, CompareType c = CompareType{}):
+                root{new Node{data}}, comparator{c} {}
+        
+        /**
+         * @brief copy constructor
+         * @param bst A reference to another bst
+         */
+        bst(const bst& bst)
+        {    
+            copy(bst.getRoot());
+        }
+        /**
+         * @brief move constructor
+         * @param bst Bst to be moved to the new one
+         */
+        bst(bst&& bst) noexcept : 
+            root{std::move(bst.getRoot())} {}
+
+        iterator begin();
+        iterator end() { return new Iterator{nullptr}; }
+        
+            //Iterator iterator;
+            //ConstIterator const_iterator;
 };
 
 #endif
