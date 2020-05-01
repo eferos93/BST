b6f39329f122463221849ea27378b03c2c154e6d
Eros Fabrici <eros.fabrici@gmail.com>
2020-05-01 12:43:10 +0200

    removed include iterator

--- include/iterator.hxx
+++ include/iterator.hxx
@@ -2,7 +2,7 @@
  * @author: Dogan Can Demirbilek
  * @author: Alessandro Scardoni
  **/
-#include <iterator>
+//#include <iterator>
 
 template<class Node, class KeyType, class ValueType, class CompareType, bool Const>
 class __iterator
